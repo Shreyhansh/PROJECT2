865428e30c62257e6ed8249aaa1f94d265f5f697
Shreyhansh <shreyhansh19@gmail.com>
2025-07-29 12:19:36 +0530

    Add files via upload

---
+++ fristprogram.py
@@ -0,0 +1,8 @@
+#Suppose you are a university student and you need to pay $4535 tuition fee for the next semester.. The college is giving you a discount of 10% on the early payment of your tuition fee.Since its a good offer , you decided to make an early payment 
+
+university_tuition_fee = 4535 
+discount = 10 
+
+fee_after_discount = (discount / 100) * university_tuition_fee
+
+print(fee_after_discount)
